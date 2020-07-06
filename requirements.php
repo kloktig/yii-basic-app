983b25dd81dc56ec180db909358cb9155c8de290
Roger <roger.h.martinsen@gmail.com>
2020-07-06 11:17:47 +0200

    fix database connection

--- requirements.php
+++ requirements.php
@@ -74,26 +74,6 @@ if (extension_loaded('gd')) {
 $requirements = array(
     // Database :
     array(
-        'name' => 'PDO extension',
-        'mandatory' => true,
-        'condition' => extension_loaded('pdo'),
-        'by' => 'All DB-related classes',
-    ),
-    array(
-        'name' => 'PDO SQLite extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('pdo_sqlite'),
-        'by' => 'All DB-related classes',
-        'memo' => 'Required for SQLite database.',
-    ),
-    array(
-        'name' => 'PDO MySQL extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('pdo_mysql'),
-        'by' => 'All DB-related classes',
-        'memo' => 'Required for MySQL database.',
-    ),
-    array(
         'name' => 'PDO PostgreSQL extension',
         'mandatory' => false,
         'condition' => extension_loaded('pdo_pgsql'),
@@ -109,13 +89,6 @@ $requirements = array(
         'memo' => extension_loaded('memcached') ? 'To use memcached set <a href="http://www.yiiframework.com/doc-2.0/yii-caching-memcache.html#$useMemcached-detail">MemCache::useMemcached</a> to <code>true</code>.' : ''
     ),
     // CAPTCHA:
-    array(
-        'name' => 'GD PHP extension with FreeType support',
-        'mandatory' => false,
-        'condition' => $gdOK,
-        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
-        'memo' => $gdMemo,
-    ),
     array(
         'name' => 'ImageMagick PHP extension with PNG support',
         'mandatory' => false,
